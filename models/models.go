a4dc7ac0455cf5641c0b33b8056e3a624ed632bb
Daniel Steinhauer <d.steinhauer@mailbox.org>
2023-02-04 03:01:45 +0100

    Sample models on startup.

--- models/models.go
+++ models/models.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"github.com/Pik-9/fixadated/util"
 	"time"
+	"errors"
+	"fmt"
 )
 
 type Availability uint8
@@ -21,7 +23,7 @@ type Participant struct {
 
 type ParticipantPlus struct {
 	Participant
-	EditId Util.Uuid `json:"editID"`
+	EditId util.Uuid `json:"editID"`
 }
 
 type Event struct {
@@ -37,22 +39,70 @@ type EventPlus struct {
 	EditId util.Uuid `json:"editID"`
 }
 
-func (prt Participant) ToJSON() string {
-	ret, _ := json.Marshal(prt)
-	return string(ret)
+var (
+	eventById map[util.Uuid]*EventPlus
+	eventByEdit map[util.Uuid]*EventPlus
+	partById map[util.Uuid]*ParticipantPlus
+	partByEdit map[util.Uuid]*ParticipantPlus
+)
+
+func init() {
+	eventById = make(map[util.Uuid]*EventPlus)
+	eventByEdit = make(map[util.Uuid]*EventPlus)
+	partById = make(map[util.Uuid]*ParticipantPlus)
+	partByEdit = make(map[util.Uuid]*ParticipantPlus)
+
+	// TODO: Load events and participations from disk
+
+	// Dummy code
+	dummy := InsertNewEvent(Event{
+		Name: "Test Event",
+		Description: "Bla bla",
+		Dates: make([]time.Time, 0),
+		Participants: make([]Participant, 0),
+	})
+
+	fmt.Println("Created dummy event with uuid", dummy.Id.ToBase64())
 }
 
-func (prt ParticipantPlus) ToJSON() string {
+func (prt Participant) ToJSON() []byte {
 	ret, _ := json.Marshal(prt)
-	return string(ret)
+	return ret
 }
 
-func (evnt Event) ToJSON() string {
+func (prt ParticipantPlus) ToJSON() []byte {
+	ret, _ := json.Marshal(prt)
+	return ret
+}
+
+func (evnt Event) ToJSON() []byte {
 	ret, _ := json.Marshal(evnt)
 	return ret
 }
 
-func (evnt EventPlus) ToJSON() string {
+func (evnt EventPlus) ToJSON() []byte {
 	ret, _ := json.Marshal(evnt)
 	return ret
+}
+
+func GetEventByUuid(uuid util.Uuid) (*EventPlus, error) {
+	ret, ok := eventById[uuid]
+	if ok {
+		return ret, nil
+	} else {
+		return nil, errors.New("Event not found.")
+	}
+}
+
+func InsertNewEvent(event Event) *EventPlus {
+	ret := EventPlus{
+		event,
+		util.RandomUuid(),
+		util.RandomUuid(),
+	}
+
+	eventById[ret.Id] = &ret
+	eventByEdit[ret.EditId] = &ret
+
+	return &ret
 }
