a1bedec93f30e9a2e249e37739539b96612415b8
Daniel Steinhauer <d.steinhauer@mailbox.org>
2023-02-05 03:10:12 +0100

    Implement route: GET /api/event/:eventid.

--- controller/eventrouter.go
+++ controller/eventrouter.go
@@ -7,6 +7,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 
 	"github.com/Pik-9/fixadated/util"
+	"github.com/Pik-9/fixadated/models"
 )
 
 func NewEventHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -22,10 +23,15 @@ func GetEventHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Param
 		return
 	}
 
-	// TODO
-	log.Println(r.Method, r.URL, eventid)
-	w.WriteHeader(501)
-	w.Write([]byte("Not implemented."))
+	event, err := models.GetEventByUuid(eventid)
+	if err != nil {
+		w.WriteHeader(404)
+		w.Write([]byte("Event not found."))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(event.GetFlatEvent().ToJSON())
 }
 
 func PatchEventHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
